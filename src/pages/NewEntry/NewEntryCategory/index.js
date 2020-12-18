1277d7f4277755b05c7dc4b37fa08c23691c6312
croschel <croschel000@gmail.com>
2020-12-18 13:44:02 -0300

    Categories are showwing on modal

--- src/pages/NewEntry/NewEntryCategory/index.js
+++ src/pages/NewEntry/NewEntryCategory/index.js
@@ -1,10 +1,22 @@
-import React, {useState} from 'react';
-import {View, Text, TouchableOpacity, Modal} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {View, Text, TouchableOpacity, Modal, FlatList} from 'react-native';
+
+import {getAllCategories} from '~/services/Categories';
 
 import styles from './styles';
 
 const NewEntryCategory = () => {
   const [modalVisible, setModalVisible] = useState(false);
+  const [allCategories, setAllCategories] = useState([]);
+
+  useEffect(() => {
+    async function loadAllCategories() {
+      const data = await getAllCategories();
+      setAllCategories(data);
+    }
+    loadAllCategories();
+    console.log('All Categories are loaded');
+  }, []);
 
   return (
     <View>
@@ -14,9 +26,24 @@ const NewEntryCategory = () => {
         <Text style={styles.pickerButtonText}>Alimentação</Text>
       </TouchableOpacity>
       <Modal animationType="slide" transparent={false} visible={modalVisible}>
-        <TouchableOpacity onPress={() => setModalVisible(false)}>
-          <Text>FECHAR</Text>
-        </TouchableOpacity>
+        <View style={styles.modal}>
+          <FlatList
+            data={allCategories}
+            keyExtractor={(item) => item.id}
+            renderItem={({item}) => (
+              <TouchableOpacity style={styles.modalItem}>
+                <Text style={[styles.modalItemText, {color: item.color}]}>
+                  {item.name}
+                </Text>
+              </TouchableOpacity>
+            )}
+          />
+          <TouchableOpacity
+            style={styles.closeButton}
+            onPress={() => setModalVisible(false)}>
+            <Text style={styles.closeButtonText}>FECHAR</Text>
+          </TouchableOpacity>
+        </View>
       </Modal>
     </View>
   );
--- src/pages/NewEntry/NewEntryCategory/styles.js
+++ src/pages/NewEntry/NewEntryCategory/styles.js
@@ -14,4 +14,36 @@ export default styles = StyleSheet.create({
     color: colors.white,
     textAlign: 'center',
   },
+  modal: {
+    flex: 1,
+    backgroundColor: colors.background,
+  },
+  modalItem: {
+    backgroundColor: colors.asphalt,
+    borderRadius: 16,
+    marginVertical: 10,
+    marginHorizontal: 20,
+    padding: 20,
+  },
+  modalItemText: {
+    fontSize: 22,
+    color: colors.white,
+    textAlign: 'center',
+  },
+  closeButton: {
+    borderColor: colors.green,
+    backgroundColor: colors.background,
+    borderWidth: 1,
+    borderRadius: 24,
+    textAlign: 'center',
+    alignSelf: 'center',
+    marginVertical: 10,
+    marginHorizontal: 20,
+    padding: 10,
+  },
+  closeButtonText: {
+    fontSize: 22,
+    color: colors.green,
+    textAlign: 'center',
+  },
 });
